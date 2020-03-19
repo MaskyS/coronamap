742ab41b1de695505c13e88c5be607412b2a92ed
MaskyS <contact@maskys.com>
2020-03-19 19:26:22 +0400

    Ensure plugins up and running

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
