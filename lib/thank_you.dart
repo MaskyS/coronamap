72f48de83bfd76d69fbfb24076d26ece0fa28099
AviSoomirtee <avisoomirtee.as@gmail.com>
2020-03-20 00:02:02 +0400

    added hotlines

--- lib/thank_you.dart
+++ lib/thank_you.dart
@@ -1,6 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart' as UrlLauncher;
 
+
+enum Hotlines { Production, Research, Purchasing, Marketing, Accounting }
+Future<Hotlines> _asyncSimpleDialog(BuildContext context) async {
+  return await showDialog<Hotlines>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: const Text('Select the nearby hospital!'),
+          children: <Widget>[
+            SimpleDialogOption(
+              onPressed: () {
+                UrlLauncher.launch("tel://8924");
+              },
+              child: const Text('Hotline - Day use'),
+            ),
+            SimpleDialogOption(
+              onPressed: () {
+                UrlLauncher.launch("tel://8925");
+              },
+              child: const Text('Hotline - Dr.A.G.Jeetoo – Port-Louis'),
+            ),
+            SimpleDialogOption(
+              onPressed: () {
+                UrlLauncher.launch("tel://8926");
+              },
+              child: const Text('Hotline - SSRN – Pamplemousses'),
+            ),
+            SimpleDialogOption(
+              onPressed: () {
+                UrlLauncher.launch("tel://8927");
+              },
+              child: const Text('Hotline - Hôpital de Flacq'),
+            ),
+            SimpleDialogOption(
+              onPressed: () {
+                UrlLauncher.launch("tel://8928");
+              },
+              child: const Text('Hotline - Victoria – Candos'),
+            ),
+            SimpleDialogOption(
+              onPressed: () {
+                UrlLauncher.launch("tel://8929");
+              },
+              child: const Text('Hotline - J.Nehru – Rose-Belle'),
+            )
+          ],
+        );
+      });
+}
+
 class ThankYouForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -23,7 +74,7 @@ class ThankYouForm extends StatelessWidget {
             child: Align(
               alignment: Alignment.bottomCenter,
               child: Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(16.0),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -40,11 +91,24 @@ class ThankYouForm extends StatelessWidget {
                       label: Text("Call SAMU"),
                       color: Colors.blue,
                     ),
+                    new RaisedButton(
+                      onPressed: () async {
+                        final Hotlines hotline = await _asyncSimpleDialog(context);
+                        //print("Selected Departement is $deptName");
+                      },
+                      splashColor: Colors.red,
+                      child: const Text(
+                      'Call Hotlines'
+                      )
+                      //child: const Text("Simple dialog"),
+                    ),
                   ],
+                  
                 ),
               ),
             ),
           ),
+
         ],
       ),
     );
