d5c2c9b6c17da2009a603fa17ae8af957ff58207
Bruno Bernard <brunobernard@cyberstorm.mu>
2020-03-19 23:13:58 +0400

    Move dart to new directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:coronamapp/symptoms_form.dart';
+import 'package:coronamapp/router.dart';
+import 'package:coronamapp/views/symptoms_form.dart';
 import 'package:flutter/material.dart';
+
+import 'constants/routes.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,38 +17,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: HomePage(),
+      onGenerateRoute: Router.generateRoute,
+      initialRoute: Routes.SPLASH_PAGE,
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  HomePage({Key key}) : super(key: key);
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("CoronaMap"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[Text("Wassup")],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        icon: Icon(Icons.warning),
-        onPressed: () => Navigator.push(
-            context, MaterialPageRoute(builder: (_) => SymptomsForm())),
-        tooltip: 'Report Symptômes',
-        label: Text("Mo Ena Symptômes"),
-        backgroundColor: Colors.orange,
-      ),
-    );
-  }
-}
