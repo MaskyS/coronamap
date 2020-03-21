4dd921fab86954f1891175df08e2d53c868121e1
MaskyS <contact@maskys.com>
2020-03-21 14:15:16 +0400

    Add geolocation logging

--- lib/models/geolocation.dart
+++ lib/models/geolocation.dart
@@ -1,15 +1,19 @@
+import 'package:geolocator/geolocator.dart';
 import 'package:json_annotation/json_annotation.dart';
 part 'geolocation.g.dart';
 
 @JsonSerializable()
 class Location {
-  String latitude;
-  String longitude;
+  double latitude;
+  double longitude;
+  String positionBasedAddress;
 
   Location({
     this.latitude,
     this.longitude,
+    this.positionBasedAddress,
   });
+
   factory Location.fromJson(Map<String, dynamic> json) =>
       _$LocationFromJson(json);
   Map<String, dynamic> toJson() => _$LocationToJson(this);
--- lib/models/geolocation.g.dart
+++ lib/models/geolocation.g.dart
@@ -8,12 +8,14 @@ part of 'geolocation.dart';
 
 Location _$LocationFromJson(Map<String, dynamic> json) {
   return Location(
-    latitude: json['latitude'] as String,
-    longitude: json['longitude'] as String,
+    latitude: (json['latitude'] as num)?.toDouble(),
+    longitude: (json['longitude'] as num)?.toDouble(),
+    positionBasedAddress: json['positionBasedAddress'] as String,
   );
 }
 
 Map<String, dynamic> _$LocationToJson(Location instance) => <String, dynamic>{
       'latitude': instance.latitude,
       'longitude': instance.longitude,
+      'positionBasedAddress': instance.positionBasedAddress,
     };
