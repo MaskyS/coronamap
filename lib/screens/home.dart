960a0548ebc719066f2d192d5cf04293c7de8572
MaskyS <contact@maskys.com>
2020-03-21 14:45:29 +0400

    Move permission request to splash

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,5 @@
 import 'package:coronamapp/constants/routes.dart';
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -25,24 +24,12 @@ class _HomePageState extends State<HomePage> {
       floatingActionButton: FloatingActionButton.extended(
         icon: Icon(Icons.warning),
         onPressed: () async {
-          PermissionStatus permissionResult =
-              await requestPermission(PermissionGroup.location);
-          if (permissionResult == PermissionStatus.granted) {
-            Navigator.pushNamed(context, Routes.formPage);
-          }
+          Navigator.pushNamed(context, Routes.formPage);
         },
         tooltip: 'Report Symptômes',
         label: Text("MO ENA SYMPTOMES"),
         backgroundColor: Colors.orange,
       ),
     );
-  }
-
-  Future<PermissionStatus> requestPermission(PermissionGroup permission) async {
-    final List<PermissionGroup> permissions = <PermissionGroup>[permission];
-    final Map<PermissionGroup, PermissionStatus> permissionRequestResult =
-        await PermissionHandler().requestPermissions(permissions);
-
-    return permissionRequestResult[permission];
   }
 }
