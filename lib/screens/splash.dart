721a0d608b9e1488c5e49466a24a505078a8e66c
MaskyS <contact@maskys.com>
2020-03-22 03:58:42 +0400

    fix error

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -6,7 +6,6 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Splash extends StatefulWidget {
-
   @override
   _SplashState createState() => _SplashState();
 }
@@ -84,19 +83,22 @@ class _SplashState extends State<Splash> {
                       ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: localeFlag.asMap().entries.map<Widget>((entry) {
+                        children:
+                            localeFlag.asMap().entries.map<Widget>((entry) {
                           int idx = entry.key;
                           var value = entry.value;
-                          return  GestureDetector(
+                          return GestureDetector(
                             onTap: () => setState(() {
-                               switchLocale(idx, value, context);
+                              switchLocale(idx, value, context);
                             }),
-                            child:  Container(
+                            child: Container(
                               decoration: BoxDecoration(
                                   shape: BoxShape.circle,
                                   boxShadow: [
                                     BoxShadow(
-                                      color: _langIndex == idx ? Colors.black54: Colors.transparent,
+                                      color: _langIndex == idx
+                                          ? Colors.black54
+                                          : Colors.transparent,
                                       blurRadius: 15.0,
                                       spreadRadius: 1.0,
                                       offset: Offset(
@@ -104,9 +106,9 @@ class _SplashState extends State<Splash> {
                                         1.0,
                                       ),
                                     )
-                                  ]
-                              ),
-                              child: Image.asset(value['flag'], height: 37, width: 37),
+                                  ]),
+                              child: Image.asset(value['flag'],
+                                  height: 37, width: 37),
                             ),
                           );
                         }).toList(),
@@ -118,15 +120,14 @@ class _SplashState extends State<Splash> {
                           icon: Icon(Icons.arrow_forward),
                           label: Text("GO"),
                           // TODO Make language matter.
-                           onPressed: () async {
-                              PermissionStatus permissionResult =
-                                  await requestPermission(
-                                      PermissionGroup.location);
-                              if (permissionResult ==
-                                  PermissionStatus.granted) {
-                                Navigator.pushNamed(context, Routes.homePage);
-                              }
-                            }),
+                          onPressed: () async {
+                            PermissionStatus permissionResult =
+                                await requestPermission(
+                                    PermissionGroup.location);
+                            if (permissionResult == PermissionStatus.granted) {
+                              Navigator.pushNamed(context, Routes.homePage);
+                            }
+                          },
                         ),
                       ),
                       Center(
@@ -140,7 +141,6 @@ class _SplashState extends State<Splash> {
                           ),
                         ),
                       ),
-
                     ],
                   ),
                 ),
@@ -152,14 +152,15 @@ class _SplashState extends State<Splash> {
     );
   }
 
-  Future switchLocale(int idx, Map<String, Object> value, BuildContext context) async {
+  Future switchLocale(
+      int idx, Map<String, Object> value, BuildContext context) async {
     _langIndex = idx;
     Locale locale = value['locale'];
-    
+
     var prefs = await SharedPreferences.getInstance();
     await prefs.setString('languageCode', locale.languageCode);
-    await prefs.setString('countryCode',  locale.countryCode);
-    
+    await prefs.setString('countryCode', locale.countryCode);
+
     MyApp.setLocale(context, locale);
   }
 
