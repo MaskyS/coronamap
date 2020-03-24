99594d3fdbf7b135dffb1238729850d3949857a1
Bernard Joseph Jean Bruno <brunobernard@cyberstorm.mu>
2020-03-24 19:24:20 +0400

    Added autopush when locale already selected (#42)

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,7 +1,8 @@
 import 'package:coronamapp/config/app_localizations.dart';
 import 'package:coronamapp/constants/routes.dart';
-import 'package:coronamapp/main.dart';
+import 'package:coronamapp/constants/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -12,6 +13,7 @@ class Splash extends StatefulWidget {
 
 class _SplashState extends State<Splash> {
   int _langIndex = 0;
+  SharedPreferences _prefs;
 
   var localeFlag = [
     {
@@ -27,9 +29,14 @@ class _SplashState extends State<Splash> {
       "locale": Locale('fr', 'FR'),
     }
   ];
+  Future<bool> _goToHomeIfLocaleAlreadySelected() async {
+    _prefs = await SharedPreferences.getInstance();
+    return _prefs.getBool(Utils.permanentLanguage);
+  }
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
@@ -54,9 +61,9 @@ class _SplashState extends State<Splash> {
                         child: Padding(
                           padding: const EdgeInsets.all(16.0),
                           child: Text(
-                            'CORONA-MAPP',
+                            'Depistaz-Mu',
                             style: TextStyle(
-                                fontWeight: FontWeight.bold,
+                                fontWeight: FontWeight.w900,
                                 letterSpacing: 3,
                                 fontSize: 24,
                                 color: Colors.black),
@@ -66,85 +73,103 @@ class _SplashState extends State<Splash> {
                     ],
                   ),
                 ),
-                Expanded(
-                  flex: 2,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      Center(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            'Language/Langaz/Langue:',
-                            style: TextStyle(
-                                color: Color.fromRGBO(128, 122, 122, 1)),
-                          ),
-                        ),
+               FutureBuilder(
+                 future: _goToHomeIfLocaleAlreadySelected(),
+                 builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    if (snapshot.data) {
+                      SchedulerBinding.instance.addPostFrameCallback((_) {
+                        Navigator.pushNamedAndRemoveUntil(context, Routes.homePage, (route) => false);
+                      });
+                   }
+                    return Expanded(
+                      flex: 2,
+                      child: Center(
+                        child: CircularProgressIndicator(),
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children:
-                            localeFlag.asMap().entries.map<Widget>((entry) {
-                          int idx = entry.key;
-                          var value = entry.value;
-                          return GestureDetector(
-                            onTap: () => setState(() {
-                              switchLocale(idx, value, context);
-                            }),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: _langIndex == idx
-                                          ? Colors.orange.withOpacity(.90)
-                                          : Colors.transparent,
-                                      blurRadius: 15.0,
-                                      spreadRadius: 2.0,
-                                      offset: Offset(
-                                        1.0,
-                                        1.0,
-                                      ),
-                                    )
-                                  ]),
-                              child: Image.asset(value['flag'],
-                                  height: 37, width: 37),
-                            ),
-                          );
-                        }).toList(),
-                      ),
-                      Center(
-                        child: RaisedButton.icon(
-                          color: Colors.red,
-                          textColor: Colors.white,
-                          icon: Icon(Icons.arrow_forward),
-                          label: Text("GO"),
-                          // TODO Make language matter.
-                          onPressed: () async {
-                            PermissionStatus permissionResult =
-                                await requestPermission(
-                                    PermissionGroup.location);
-                            if (permissionResult == PermissionStatus.granted) {
-                              Navigator.pop(context);
-                            }
-                          },
-                        ),
-                      ),
-                      Center(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            'With geolocation/Avek ou position/Avec Géolocalisation',
-                            style: TextStyle(
-                              fontSize: 8.0,
-                              color: Color.fromRGBO(128, 122, 122, 0.8),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                    );
+                  }
+                   return  Expanded(
+                     flex: 2,
+                     child: Column(
+                       mainAxisAlignment: MainAxisAlignment.spaceAround,
+                       children: <Widget>[
+                         Center(
+                           child: Padding(
+                             padding: const EdgeInsets.all(8.0),
+                             child: Text(
+                               'Language/Langaz/Langue:',
+                               style: TextStyle(
+                                   color: Color.fromRGBO(128, 122, 122, 1)),
+                             ),
+                           ),
+                         ),
+                         Row(
+                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                           children:
+                           localeFlag.asMap().entries.map<Widget>((entry) {
+                             int idx = entry.key;
+                             var value = entry.value;
+                             return GestureDetector(
+                               onTap: () => setState(() {
+                                 switchLocale(idx, value, context);
+                               }),
+                               child: Container(
+                                 decoration: BoxDecoration(
+                                     shape: BoxShape.circle,
+                                     boxShadow: [
+                                       BoxShadow(
+                                         color: _langIndex == idx
+                                             ? Colors.orange.withOpacity(.90)
+                                             : Colors.transparent,
+                                         blurRadius: 15.0,
+                                         spreadRadius: 2.0,
+                                         offset: Offset(
+                                           1.0,
+                                           1.0,
+                                         ),
+                                       )
+                                     ]),
+                                 child: Image.asset(value['flag'],
+                                     height: 37, width: 37),
+                               ),
+                             );
+                           }).toList(),
+                         ),
+                         Center(
+                           child: RaisedButton.icon(
+                             color: Colors.red,
+                             textColor: Colors.white,
+                             icon: Icon(Icons.arrow_forward),
+                             label: Text("GO"),
+                             // TODO Make language matter.
+                             onPressed: () async {
+                               PermissionStatus permissionResult =
+                               await requestPermission(
+                                   PermissionGroup.location);
+                               if (permissionResult == PermissionStatus.granted) {
+                                 Navigator.pop(context);
+                               }
+                             },
+                           ),
+                         ),
+                         Center(
+                           child: Padding(
+                             padding: const EdgeInsets.all(8.0),
+                             child: Text(
+                               'With geolocation/Avek ou position/Avec Géolocalisation',
+                               style: TextStyle(
+                                 fontSize: 8.0,
+                                 color: Color.fromRGBO(128, 122, 122, 0.8),
+                               ),
+                             ),
+                           ),
+                         ),
+                       ],
+                     ),
+                   );
+                 },
+               )
               ],
             ),
           )
@@ -158,11 +183,12 @@ class _SplashState extends State<Splash> {
     _langIndex = idx;
     Locale locale = value['locale'];
 
-    var prefs = await SharedPreferences.getInstance();
-    await prefs.setString('languageCode', locale.languageCode);
-    await prefs.setString('countryCode', locale.countryCode);
+    _prefs = await SharedPreferences.getInstance();
+    await _prefs.setString('language_code', locale.languageCode);
+    await _prefs.setString('country_code', locale.countryCode);
+    await _prefs.setBool(Utils.permanentLanguage, true);
 
-    MyApp.setLocale(context, locale);
+    AppLocalizations.load(locale);
   }
 
   Future<PermissionStatus> requestPermission(PermissionGroup permission) async {
